ee8cacbeda7ef1f609ba2a3780bd6a84e6f26efd
kdhima <krisdhima@gmail.com>
2021-10-11 15:04:35 +0200

    Automatic update

---
+++ Approche_Obj/MyShop/MyShop.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MyShop {
+    private ArrayList<Stock> stocks = new ArrayList<Stock>();
+
+    public ArrayList<Stock> getStocks() {
+        return stocks;
+    }
+}
---
+++ Approche_Obj/MyShop/Stock.java
@@ -0,0 +1,44 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Stock  extends ArrayList<Product>{
+    private String name;
+    private String addr;
+
+    public Stock(String name) {
+        this.name = name;
+    }
+    public ArrayList<Product> getProducts(){
+        return this;
+    }
+    public void addFood(String name, int quantity, String BBD){
+        LocalDate dateLC = null;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MM/yyyy");
+        try{
+            dateLC = LocalDate.parse(BBD,formatter);
+        }
+        catch (Exception e){
+            System.out.println("Saisissez une bonne date s'il vous plait !\n");
+        }
+        if(dateLC != null) {
+            add(new Food(name, quantity, dateLC));
+            System.out.println("Produit alimentaire '" + name + "' ajouté!\n");
+        }
+    }
+    public void addSanitary(String name, int quantity){
+        add(new Sanitary(name,quantity));
+        System.out.println("Produit sanitaire '" + name +"' ajouté!\n");
+    }
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "[ name: " + name + ", nombre d'articles : " + size() + "]\n";
+    }
+}
