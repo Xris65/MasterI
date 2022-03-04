a19a42a34ee4e3d26cb03f4850be14d5dbf55adb
kdhima <kristo.dhima@etu.u-bordeaux.fr>
2022-03-05 00:01:29 +0100

    Automatic Update

---
+++ GL_S8/CF/.frama-c/frama_c_journal.ml
@@ -0,0 +1,41 @@
+(* Frama-C journal generated at 10:43 the 02/03/2022 *)
+
+exception Unreachable
+exception Exception of string
+
+[@@@ warning "-26"]
+
+(* Run the user commands *)
+let run () =
+  Dynamic.Parameter.String.set ""
+    "/net/cremi/kdhima/Master/GL_S8/CF/tp1/Exo1/first-example.c";
+  File.init_from_cmdline ();
+  Project.set_keep_current false;
+  Dynamic.Parameter.String.set "-wp-cache" "none";
+  ()
+
+(* Main *)
+let main () =
+  Journal.keep_file
+     (Datatype.Filepath.of_string
+     (".frama-c/frama_c_journal.ml"));
+  try run ()
+  with
+  | Unreachable -> Kernel.fatal "Journal reaches an assumed dead code" 
+  | Exception s -> Kernel.log "Journal re-raised the exception %S" s
+  | exn ->
+    Kernel.fatal
+      "Journal raised an unexpected exception: %s"
+      (Printexc.to_string exn)
+
+(* Registering *)
+let main : unit -> unit =
+  Dynamic.register
+    ~plugin:"Frama_c_journal.ml"
+    "main"
+    (Datatype.func Datatype.unit Datatype.unit)
+    ~journalize:false
+    main
+
+(* Hooking *)
+let () = Cmdline.run_after_loading_stage main; Cmdline.is_going_to_load ()
