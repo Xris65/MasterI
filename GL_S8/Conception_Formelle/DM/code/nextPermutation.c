054de0ee459e254b7d717926b7d6563f11c1c796
Kristo Dhima <kristo.dhima@etu.u-bordeaux.fr>
2022-05-01 15:39:20 +0200

    Automatic Update

--- GL_S8/Conception_Formelle/DM/code/nextPermutation.c
+++ GL_S8/Conception_Formelle/DM/code/nextPermutation.c
@@ -8,20 +8,43 @@ bool NextPermutation(int *P, unsigned int n)
 
     /* étape 1: cherche i le plus grand tq P[i]<P[i+1] */
     j = n - 1;
+
+    /*@ loop invariant 0 <= j <= n - 1;
+        loop invariant \forall integer k ; j < k <= n - 1 ==> P[k - 1] >= P[k]; 
+        loop assigns j;
+        loop variant j;
+    */
     while (j > 0 && P[j - 1] >= P[j])
     {
         j--;
     }
+    //@ assert j == 0 || P[j-1] < P[j];
+
+
     /* on a trouvé un i tq P[i]<P[i+1] */
     if (j == 0)
     {
         return false; /* le plus grand i tq P[i]<[i+1] n'existe pas */
     }
+    //@ assert j <= n - 1;
+    //@ assert P[j-1] < P[j];
+
+
     i = j - 1;
+    //@ assert i < n;
+    //@ assert P[i] < P[i+1];
+
     /* étape 2: cherche j le plus grand tq P[i]<P[j] */
+
+    /*@ loop invariant i < j <= n;
+        loop invariant \forall integer k ; i < k < j  ==> P[i] < P[k]; 
+        loop assigns j;
+        loop variant n - j;
+    */
     while ((j < n) && (P[i] < P[j]))
         j++;
     j--;
+    
     swap(P + i, P + j);
     //@ assert isPermutation(P,n);
     reverse(P, i + 1, n - 1, n);
--- GL_S8/Conception_Formelle/DM/code/nextPermutation.h
+++ GL_S8/Conception_Formelle/DM/code/nextPermutation.h
@@ -1,7 +1,26 @@
 #include "formalism.h"
 #include "limits.h"
 
-/*
-TODO
+/*@ requires 0 < n < INT_MAX;
+    requires \valid (P+(0..n-1));
+    requires isPermutation(\at(P, Pre), n);
+
+    terminates \true;
+
+    ensures isPermutation(\at(P, Post), n);
+    assigns P[0..n-1];
+
+    behavior MaxPerm:
+        assumes isMaxPerm{Pre}(P,n);
+        ensures \result == false;
+        ensures unchangedTab{Pre,Post}(P, P, 0, n);
+    behavior NotMaxPerm:
+        assumes !isMaxPerm{Pre}(P,n);
+        ensures \result == true;
+        ensures isStrictlyBiggerPerm{Pre,Post}(P, P, n);
+    
+    complete behaviors;
+    disjoint behaviors MaxPerm,NotMaxPerm;
+
 */
 bool NextPermutation(int *P, unsigned int n);
